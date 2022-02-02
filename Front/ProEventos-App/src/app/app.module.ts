f1ced4bc64e865e3ec6c52b45e6aa5d8c50baf95
Esdras Correa <esdras_correa@hotmail.com>
2022-02-02 01:03:21 -0300

    seção 4 Angular Atualização

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,12 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule} from "@angular/common/http"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +17,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -16,10 +16,10 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
-  public getEventos(): void {
+  public getEventos(): void{
     this.http.get('https://localhost:5001/api/eventos').subscribe(
       response => this.eventos = response,
       error => console.log(error)
-    );
+    )
   }
 }
